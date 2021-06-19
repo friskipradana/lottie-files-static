9bcb1d79bd9a5e2680934c8ab1842fadee327b18
friskipradana <friskipradana8@gmail.com>
2021-06-19 11:11:01 +0700

    basic lottiefiles

---
+++ script/index.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('bm'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'https://assets8.lottiefiles.com/packages/lf20_6k4jsmai.json'
+})
